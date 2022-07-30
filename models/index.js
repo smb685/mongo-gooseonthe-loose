39227aea69a271e91f251a563f8080136ad55144
Sylvester Brandon <sylvester.brandon25@gmail.com>
2022-07-30 10:55:29 -0500

    adding the rest of the files with updates

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Thought = require('./Thought');
+const { model } = require('mongoose');
+
+model.exports = {
+    User, Thought
+}
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {
+  getThoughtById,
+  getAllThoughts,
+  addThought,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction, } = require('../..controllers/thought-controller');
+
+  router
+  .route('/')
+  .get(getAllThoughts)
+  .post(addThought);
+
+router
+  .route('/:thoughtId')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(removeThought);
+
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction);
+
+router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction);
+
+module.exports = router;
